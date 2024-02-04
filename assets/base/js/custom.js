e13f403692dd371110e5b89c20f66d66955efb54
yousefhussien727 <yousef.hu@outlook.com>
2024-02-04 15:56:06 +0200

    add dynamic language toggle js function in EN pages

---
+++ assets/base/js/custom.js
@@ -0,0 +1,21 @@
+/**
+ Custom JS
+ **/
+
+// BEGIN: Toggle Language Button Function
+function toggleLanguage() {
+    // Get the current page's URL splitted in array
+    var currentUrlSplited = window.location.pathname.split('/');
+
+    // Check the current language and toggle to the opposite
+    var newLanguage = (currentUrlSplited[1]=='en') ? 'ar' : 'en';
+    // console.log(newLanguage);
+
+    // Construct the new URL for the toggled language
+    var newUrl = '../' + newLanguage + '/pages/' + currentUrlSplited[2];
+    // console.log(newUrl);
+
+    // Set the href attribute of the link to the new URL
+    document.getElementById('languageToggleLink').href = newUrl;
+}
+// END
